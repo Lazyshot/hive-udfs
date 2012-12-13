071fde1f2fce7e6a0b81af31c75d21171dc17c17
Bryan <lazyshot@gmail.com>
2012-12-13 13:49:10 -0500

    adding function description to array_intersect

--- src/main/java/com/louddoor/hiveudfs/ArrayIntersect.java
+++ src/main/java/com/louddoor/hiveudfs/ArrayIntersect.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections.ListUtils;
+import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 
-
+@Description(name="array_intersect", value="_FUNC_(arg,arg) - Returns map of all the key/value pairs")
 public class ArrayIntersect extends UDF {
 	
 	@SuppressWarnings("rawtypes")
