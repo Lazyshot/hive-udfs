4d34f92e202443c196c936c8cb47dcacfe05fc7a
Bryan <lazyshot@gmail.com>
2012-12-13 15:42:46 -0500

    converting to ant

--- src/main/java/com/louddoor/hiveudfs/ArrayIntersect.java
+++ src/main/java/com/louddoor/hiveudfs/ArrayIntersect.java
@@ -6,8 +6,22 @@ import java.util.List;
 import org.apache.commons.collections.ListUtils;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hive.pdk.HivePdkUnitTests;
+import org.apache.hive.pdk.HivePdkUnitTest;
 
 @Description(name="array_intersect", value="_FUNC_(arg,arg) - Returns map of all the key/value pairs")
+@HivePdkUnitTests(setup = "create table dual_data (i int);"
+		+ "insert overwrite table dual_data select 'Facebook' from onerow limit 1;",
+		cleanup = "drop table if exists dual_data;",
+		cases = {
+				@HivePdkUnitTest(
+					query = "SELECT array_intersect(array(1,2,3,4), array(2,4)) FROM onerow;",
+					result = ""),
+				@HivePdkUnitTest(
+					query = "SELECT array_intersect(array(1,2,3), array(4,5)) FROM onerow;",
+					result = "")
+			}
+		)
 public class ArrayIntersect extends UDF {
 	
 	@SuppressWarnings("rawtypes")
